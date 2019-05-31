01fda6d6e37ffbace2a71cc05b9e5f6db70d1d52
stmball <stmball@gmail.com>
2019-05-31 15:59:21 +0100

    Moving files

--- mathsQuiz/urls.py
+++ mathsQuiz/urls.py
@@ -19,7 +19,7 @@ from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('/', views.index, name='index'),
+    path('', views.index, name='index'),
     path('<str:quizName>/', views.quiz, name='quiz'),
     path('scoring/', views.scoring, name='scoring'),
 ]
